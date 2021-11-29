55ae2a651b46ac50b941338cbee2aa8b30ff0f4d
tcrear <smileybear88@hotmail.com>
2021-11-29 01:07:50 -0600

    updating routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,87 @@
-const router = require("express").Router();
-const postRoutes = require('./postRoutes');
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
 
-router.use('/posts', postRoutes);
+router.get('/users', async(req, res) => {
+    try {
+        const userData = await User.findAll({
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.post('/new-user', async (req, res) => {
+    console.log(req.body);
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+
+router.post('/', async(req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/user/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username } });
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'Logged In' });
+        });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/user/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.get('/newuser', async (req, res) => {
+    res.render('newuser');
+})
+
 
 module.exports = router;
--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -1,9 +1,58 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const { Post, Comment } = require('../../models');
 
-const {
-    getPosts
-} = require('../../controllers/postController');
+module.exports = router;
 
-router.route('/').get(getPosts)
 
-module.exports = router
+router.post('/new-post', async (req, res) => {
+    try {
+        const postData = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.body.user_id,
+
+
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+router.get('/blog-posts', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.post('/new-comment', async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            content: req.body.content,
+            user_id: req.body.user_id,
+            blog_id: req.body.blog_id
+
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+router.get('/comments', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
