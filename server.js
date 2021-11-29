9fde1434ee7b43f56cbde8d15d33da6f0083bc41
tcrear <smileybear88@hotmail.com>
2021-11-29 01:02:58 -0600

    updating server file.

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
+const path = require('path');
 const express = require('express');
-// const session = require('express-session');
-const routes = require('./routes');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const models = require('./models/index');
+
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers')
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({});
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 };
 
-// app.use(session(sess));
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
